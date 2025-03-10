6b5800767a58b5d18e1732db5cf977d12fe6de36
danijel-enoch <enochdanijel@gmail.com>
2025-03-10 15:04:21 +0100

    Enhance binary options program with additional state management and prediction functionalities; update dependencies and initialization logic

--- programs/binary_options/src/lib.rs
+++ programs/binary_options/src/lib.rs
@@ -1,32 +1,173 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Token, TokenAccount};
-
-declare_id!("5k6s5WeXsT1HFpKRGbwnFa2rSttUBYWzVHupJrrLgSj");
-
+use anchor_spl::{
+    token::{
+        Transfer as TransferSPL, Token, TokenAccount, Mint, transfer as transfer_spl,
+    },
+    associated_token::AssociatedToken,
+};
+declare_id!("HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1");
 #[program]
-pub mod bin_ops {
+pub mod binary_options {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<InitializeContext>) -> Result<()> {
+        ctx.accounts.state.total_xyz_balance = 0;
+        ctx.accounts.state.fee_percentage = 0;
+        ctx.accounts.state.admin = ctx.accounts.user.key();
+        ctx.accounts.state.xyz_mint = ctx.accounts.xyz_mint.key();
+        ctx.accounts.state.auth_bump = ctx.bumps.auth;
+        ctx.accounts.state.prediction_counter = 1;
+        Ok(())
+    }
+    pub fn create_prediction(
+        ctx: Context<CreatePredictionContext>,
+        amount: u64,
+        token_mint: Pubkey,
+        start_timestamp: u64,
+        expiry_timestamp: u64,
+        start_price: u64,
+        end_price: u64,
+        prediction_type: String,
+    ) -> Result<()> {
+        let cpi_accounts = TransferSPL {
+            from: ctx.accounts.maker_ata.to_account_info(),
+            to: ctx.accounts.xyz_vault.to_account_info(),
+            authority: ctx.accounts.user.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            cpi_accounts,
+        );
+        transfer_spl(cpi_ctx, amount)?;
+        ctx.accounts.prediction_state.amount = amount;
+        ctx.accounts.prediction_state.token_mint = token_mint;
+        ctx.accounts.prediction_state.start_timestamp = start_timestamp;
+        ctx.accounts.prediction_state.expiry_timestamp = expiry_timestamp;
+        ctx.accounts.prediction_state.start_price = start_price;
+        ctx.accounts.prediction_state.end_price = end_price;
+        ctx.accounts.prediction_state.prediction_type = prediction_type;
+        ctx.accounts.prediction_state.trader = ctx.accounts.maker_ata.key();
+        Ok(())
+    }
+    pub fn settle_prediction(
+        ctx: Context<SettlePredictionContext>,
+        is_winning: bool,
+        id: u64,
+        taker: Pubkey,
+    ) -> Result<()> {
         Ok(())
     }
 }
-
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + BinaryOptionsMainState::INIT_SPACE)]
-    pub state: Account<'info, BinaryOptionsMainState>,
+pub struct InitializeContext<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 130,
+        seeds = [b"binary_options",
+        auth.key().as_ref()],
+        bump,
+    )]
+    pub state: Account<'info, BinaryOptionsState>,
+    #[account(seeds = [b"auth"], bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub xyz_mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        seeds = [b"vault",
+        state.key().as_ref()],
+        token::mint = xyz_mint,
+        token::authority = auth,
+        bump,
+    )]
+    pub xyz_vault: Account<'info, TokenAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
 }
-
+#[derive(Accounts)]
+pub struct CreatePredictionContext<'info> {
+    #[account(init, payer = user, space = 156, seeds = [b"prediction"], bump)]
+    pub prediction_state: Account<'info, PredictionState>,
+    #[account(mut)]
+    pub xyz_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(seeds = [b"auth"], bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        associated_token::mint = xyz_mint,
+        associated_token::authority = user,
+    )]
+    pub maker_ata: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [b"vault",
+        state.key().as_ref()],
+        token::mint = xyz_mint,
+        token::authority = auth,
+        bump,
+    )]
+    pub xyz_vault: Account<'info, TokenAccount>,
+    #[account(seeds = [b"binary_options", auth.key().as_ref()], bump)]
+    pub state: Account<'info, BinaryOptionsState>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct SettlePredictionContext<'info> {
+    #[account(mut, seeds = [b"prediction"], bump)]
+    pub prediction_state: Account<'info, PredictionState>,
+    #[account(
+        mut,
+        seeds = [b"vault",
+        state.key().as_ref()],
+        token::mint = xyz_mint,
+        token::authority = auth,
+        bump,
+    )]
+    pub xyz_vault: Account<'info, TokenAccount>,
+    #[account(seeds = [b"auth"], bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    #[account(seeds = [b"binary_options", auth.key().as_ref()], bump)]
+    pub state: Account<'info, BinaryOptionsState>,
+    #[account()]
+    pub taker_receive_ata: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub xyz_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
 #[account]
-#[derive(InitSpace)]
-pub struct BinaryOptionsMainState {
-    pub admin: Pubkey,
+pub struct PredictionState {
+    pub user: Pubkey,
+    pub amount: u64,
+    pub trader: Pubkey,
     pub token_mint: Pubkey,
-    pub fee_percentage: u8,
+    pub start_timestamp: u64,
+    pub expiry_timestamp: u64,
+    pub start_price: u64,
+    pub end_price: u64,
+    pub prediction_type: String,
+    pub is_settled: bool,
+    pub is_winning: bool,
+}
+#[account]
+pub struct BinaryOptionsState {
+    pub admin: Pubkey,
+    pub prediction_counter: u64,
+    pub xyz_mint: Pubkey,
+    pub xyz_vault: Pubkey,
+    pub total_xyz_balance: u64,
+    pub fee_percentage: u64,
+    pub auth_bump: u8,
+    pub vault_bump: u8,
 }
--- ts_contracts/index.ts
+++ ts_contracts/index.ts
@@ -4,7 +4,18 @@ import {
 	Result,
 	i64,
 	u8,
-	Signer
+	Signer,
+	u64,
+	TokenAccount,
+	UncheckedAccount,
+	Boolean,
+	Str,
+	f64,
+	u128,
+	AssociatedTokenAccount,
+	Mint,
+	TokenProgram,
+	Seeds
 } from "@solanaturbine/poseidon";
 
 // creating a class VoteProgram is similar to creating a creating a mod in anchor with all the instructions inside
@@ -15,31 +26,129 @@ export default class BinaryOptions {
 	);
 
 	// we can pass in standard Accounts(Signer, TokenAccount, Mint, UncheckedAccount and so on), Custom Accounts(state in this case) and IX arguements(hash in this case) as parameters.
-	initialize(state: VoteState, user: Signer): Result {
+	initialize(
+		state: BinaryOptionsState,
+		user: Signer,
+		xyzVault: TokenAccount,
+		auth: UncheckedAccount,
+		xyzMint: Mint
+	): Result {
 		// PDAs can be derived like <custom_Acc>.derive([...])
 		// where inside array we can pass string, Uint8Array, pubkey
 		// we can also derive PDAs which are token account, associated token account which will be covered in vault and escrow
-		state.derive(["vote"]).init(user); // we can initialise PDA just by chaining a init method to the derive method
-
+		auth.derive(["auth"]);
+		state.derive(["binary_options", auth.key]).init(user); // we can initialise PDA just by chaining a init method to the derive method
+		xyzVault.derive(["vault", state.key], xyzMint, auth.key);
 		// defining properties(vote) of custom_Acc(state)
-		state.vote = new i64(0);
+		state.total_xyz_balance = new u64(0);
+		state.fee_percentage = new u64(0);
+		state.admin = user.key;
+		state.xyz_mint = xyzMint.key;
+		state.authBump = auth.getBump();
+		state.prediction_counter = new u64(1);
 	}
-	claim_rewards(): Result {}
-	settle_prediction(): Result {
-		// only admin can settle prediction
-	}
-	create_prediction(): Result {
+	create_prediction(
+		user: Signer,
+		auth: UncheckedAccount,
+		state: BinaryOptionsState,
+		xyz_mint: Mint,
+		prediction_state: PredictionState,
+		makerAta: AssociatedTokenAccount,
+		xyz_vault: TokenAccount,
+		amount: u64,
+		token_mint: Pubkey,
+		start_timestamp: u64,
+		expiry_timestamp: u64,
+		start_price: u64,
+		end_price: u64,
+		prediction_type: Str<6>
+	): Result {
+		auth.derive(["auth"]);
+		state.derive(["binary_options", auth.key]);
+		xyz_vault.derive(["vault", state.key], xyz_mint, auth.key);
 		// create prediction by sending funds into smart contract
+		makerAta.derive(xyz_mint, user.key);
+		prediction_state
+			.derive(["prediction", state.prediction_counter])
+			.init(user);
+		TokenProgram.transfer(
+			makerAta, // from
+			xyz_vault, // to
+			user, // authority
+			amount // amount to transferred
+		);
+
+		prediction_state.amount = amount;
+		prediction_state.token_mint = token_mint;
+		prediction_state.start_timestamp = start_timestamp;
+		prediction_state.expiry_timestamp = expiry_timestamp;
+		prediction_state.start_price = start_price;
+		prediction_state.end_price = end_price;
+		prediction_state.prediction_type = prediction_type;
+		prediction_state.trader = makerAta.key;
+		prediction_state.is_settled = false;
+		prediction_state.is_winning = false;
+		//
+		state.prediction_counter.add(1);
 	}
-	withdraw_fees(): Result {
-		// withdraw fees from smart contract
-		// only admin can withdraw fees
+	settle_prediction(
+		auth: UncheckedAccount,
+		state: BinaryOptionsState,
+		prediction_state: PredictionState,
+		xyzVault: TokenAccount,
+		takerReceiveAta: TokenAccount,
+		user: Signer,
+		xyz_mint: Mint,
+		is_winning: Boolean,
+		id: u64,
+		taker: Pubkey
+	): Result {
+		auth.derive(["auth"]);
+		state.derive(["binary_options", auth.key]);
+		prediction_state.derive(["prediction", id]);
+
+		prediction_state.is_settled = true;
+		//prediction_state.is_winning = is_winning;
+		xyzVault.derive(["vault", state.key], xyz_mint, auth.key);
+		let seeds: Seeds = ["auth", state.authBump.toBytes()];
+		// TokenProgram.transfer(
+		// 	xyzVault, // from
+		// 	takerReceiveAta, // to
+		// 	auth, // authority
+		// 	prediction_state.amount, // amount to be sent
+		// 	seeds // seeds will be at the last arguments if needed
+		// );
+		// only admin can settle predicti	on
+		// check user has been settled
+		// settle user by sending tokens to their account
+		// send to user original input amount - 2 percent fees
+		// send to user 15 %
 	}
 }
 
 // define custom accounts by creating an interface which extends class Account
-export interface VoteState extends Account {
-	// a variety of types are available like u8-u128, i8-i128, usize, boolean, string, Pubkey, etc...
-	vote: i64;
-	bump: u8;
+
+export interface BinaryOptionsState extends Account {
+	admin: Pubkey;
+	prediction_counter: u64;
+	xyz_mint: Pubkey;
+	xyz_vault: Pubkey;
+	total_xyz_balance: u64;
+	fee_percentage: u64;
+	authBump: u8;
+	vaultBump: u8;
+}
+
+export interface PredictionState extends Account {
+	user: Pubkey;
+	amount: u64;
+	trader: Pubkey;
+	token_mint: Pubkey;
+	start_timestamp: u64;
+	expiry_timestamp: u64;
+	start_price: u64;
+	end_price: u64;
+	prediction_type: Str<6>;
+	is_settled: Boolean;
+	is_winning: Boolean;
 }
